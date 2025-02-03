a94a40faaa69fa930bc7835e48a42d1022e08213
Tyler Johnson <tylerj0937@gmail.com>
2025-02-03 18:24:17 -0500

    add styling for copy button

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -1,11 +1,12 @@
 import '../App.css'
 import { useState, useEffect } from 'react';
+import { PiCopyLight } from "react-icons/pi";
 
 const Clipboard = () => {
   const [fullName, setFullName] = useState('');
   const [first, setFirst] = useState('');
   const [last, setLast] = useState('');
-  const [address, setAddress] = useState('');
+  const [address, setAddress] = useState('1234 Address Street');
   const [textToCopy, setTextToCopy] = useState('defaultText');
 
   const handleCopy = (textToCopy) => {
@@ -14,7 +15,16 @@ const Clipboard = () => {
   }
   return (
     <>
-      <button onClick={()=> handleCopy('Text')}>Copy Text</button>
+      <div className="flex">
+        
+        <input
+          type="text"
+          value={address}
+          readOnly
+        />
+        <PiCopyLight className="align-bottom mt-2 font-bold text-2xl" onClick={() => handleCopy(address)} />
+        <button className="bg-yellow-400 text-black px-2 ml-4">Edit</button>
+      </div>
     </>
   )
 }
