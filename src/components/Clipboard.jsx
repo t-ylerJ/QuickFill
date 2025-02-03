066a276dfde4136f10b26b40b6814b3b49181081
Tyler Johnson <tylerj0937@gmail.com>
2025-02-03 18:59:34 -0500

    implement ability to edit field

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -8,25 +8,44 @@ const Clipboard = () => {
   const [last, setLast] = useState('');
   const [address, setAddress] = useState('1234 Address Street');
   const [textToCopy, setTextToCopy] = useState('defaultText');
+  const [isEditable, setIsEditable] = useState(false);
 
   const handleCopy = (textToCopy) => {
     navigator.clipboard.writeText(textToCopy)
-      .then(()=> (console.log('copied text')))
+      .then(() => (console.log('copied text')));
   }
+  const handleEdit = () => {
+    setIsEditable(!isEditable);
+  }
+  const handleChange = (e) => {
+    setAddress(e.target.value)
+  }
+  //handle edit toggles ability to edit field
+  //passed as onclick function via edit button
+  // when in edit mode, readOnly is false
+  // input field can be edited
+  //  edit button becomes save button
+  //
   return (
     <>
       <div className="flex">
-        
         <input
           type="text"
           value={address}
-          readOnly
+          readOnly={!isEditable}
+          onChange={handleChange}
         />
         <PiCopyLight className="align-bottom mt-2 font-bold text-2xl" onClick={() => handleCopy(address)} />
-        <button className="bg-yellow-400 text-black px-2 ml-4">Edit</button>
+        <button
+          onClick={handleEdit}
+          className={`${isEditable ? "bg-green-500 text-white" : "bg-yellow-400 text-black"
+            } px-2 ml-4`}
+        >
+          {isEditable ? "Save" : "Edit"}
+        </button>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Clipboard;
