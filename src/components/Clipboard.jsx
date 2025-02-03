56a4ab511ee37542a3f08225a1487a198cad60ea
Tyler Johnson <tylerj0937@gmail.com>
2025-02-02 20:49:00 -0500

    implement copy function

--- src/components/Clipboard.jsx
+++ src/components/Clipboard.jsx
@@ -6,10 +6,15 @@ const Clipboard = () => {
   const [first, setFirst] = useState('');
   const [last, setLast] = useState('');
   const [address, setAddress] = useState('');
+  const [textToCopy, setTextToCopy] = useState('defaultText');
 
+  const handleCopy = (textToCopy) => {
+    navigator.clipboard.writeText(textToCopy)
+      .then(()=> (console.log('copied text')))
+  }
   return (
     <>
-      <button onClick={() => { navigator.clipboard.writeText(this.state.textToCopy) }}>Copy Text</button>
+      <button onClick={()=> handleCopy('Text')}>Copy Text</button>
     </>
   )
 }
